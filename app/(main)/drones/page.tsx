922f92651aaafa4ed19c361d3afd029e70927809
GuoSurui <thuray.email@gmail.com>
2025-04-14 10:47:30 +0800

    style: 修改搜索按钮样式和图标

--- app/(main)/drones/page.tsx
+++ app/(main)/drones/page.tsx
@@ -33,6 +33,7 @@ import {
 import { useMemo, useState } from "react";
 import DeleteDialog from "./delete-dialog";
 import DetailDialog from "./detail-dialog";
+import { Search } from "lucide-react";
 
 const columnHelper = createColumnHelper<DroneItemResult>();
 
@@ -198,7 +199,9 @@ export default function DronesPage() {
         <Button
           onClick={() => listQuery.refetch()}
           disabled={listQuery.isPending}
+          className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
         >
+          <Search className="h-4 w-4" />
           搜索
         </Button>
         <div className="flex-1"></div>
